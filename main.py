3b6e48f41f2eca1816c38f74edbfe0a13111dfb3
CyberFights <shadowjer011@gmail.com>
2026-04-17 17:12:51 -0400

    Change FastAPI server port from 8000 to 3000

--- main.py
+++ main.py
@@ -45,6 +45,6 @@ async def music_queue():
 
 if __name__ == "__main__":
     # Inicia la aplicación FastAPI
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
 
 
